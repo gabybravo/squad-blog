087555fba2c966ede5c7ae246bf772be6e1738e2
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-05-07 21:59:25 -0300

    Reformulé el código otra vez, ahora el constructor contiene un método que es el que muestra los elementos en el HTML. Se elimina la función que contiene a las compañeras y estas se dejan declaradas solamente, se guardan en un arreglo y se muestran a través del método creado en el constructor. Aún tiene errores que corregir y falta desarrollar el botón Like.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,60 +1,59 @@
-var arr = [];
-var comentarios = [];
-
 function MiembroSquad(nombreApellido, edad, hobbies){
 	this.nombreApellido = nombreApellido;
 	this.edad = edad;
 	this.hobbies = hobbies;
+	this.mostrar = function (){
+		var imprimir = document.getElementById("compas");
+		squad.forEach(function(el){
+		var divVacio = document.createElement("div");
+		divVacio.className = "usuario";
+		divVacio.innerHTML += "<br>" + "<b>Nombre:</b> " + el.nombreApellido + "<br><b>Edad:</b> " + el.edad + "<br><b>Hobbies:</b><br>";
+		imprimir.appendChild(divVacio) 
+		var lista = document.createElement("li");
+		lista.innerHTML = el.hobbies.forEach(function(e){
+		divVacio.innerHTML += "<li>" + e + "</li>";
+		});
+		var divComentarios = document.createElement("div");
+		divComentarios.className = "comentarios";
+		var form = document.createElement("form");
+		var input = document.createElement("textarea");
+		var boton = document.createElement("button");
+		var botonTexto = document.createTextNode("Agregar comentario");
+		boton.appendChild(botonTexto);
+		boton.className = "boton1";
+		input.id = "comentarioPara" + squad.indexOf(this);
+		input.placeholder = "Escribe aquí un comentario..."
+		form.appendChild(input);
+		form.appendChild(boton);
+		var comentarios = document.createElement("b");
+		var comentariosTexto = document.createTextNode("Comentarios: ");
+		comentarios.appendChild(comentariosTexto);
+		var divComentariosUsuario = document.createElement("div");
+		divComentariosUsuario.id = "comentarios" + squad.indexOf(this);
+		boton.onclick = function agregarComentario(){
+			document.getElementById(divComentariosUsuario.id).innerHTML += '<p>' + input.value + "</p><button onclick=like()>Like</button>";
+				return false;
+			};
+		divComentarios.appendChild(form);
+		divComentarios.appendChild(comentariosTexto);
+		divComentarios.appendChild(divComentariosUsuario);
+		divVacio.appendChild(divComentarios);
+		imprimir.appendChild(divVacio);
+
+		});
+	}
+
 }
 
-function Comentario(id_miembro,comentario,likes){
-	this.id_miembro = id_miembro;
-	this.comentario = comentario;
-	this.likes = likes;
-}
+var gaby = new MiembroSquad("Gaby Bravo", "28 años", ["Pintar", "Ver anime", "Jugar Aion"])
+var karina = new MiembroSquad("Karina Laroze", "21 años", ["Bailar"])
+var mily = new MiembroSquad("Mily Rivas", "18 años", ["Leer", "Ver películas"])
+var marcela = new MiembroSquad("Marcela Orellana", "31 años", ["Andar en bicicleta", "Ver series coreanas"])
+var angela = new MiembroSquad("Angela Aliaga", "32 años", ["Leer", "Escuchar música", "Jugar Donkey Kong Country"])
+var dani = new MiembroSquad("Daniela Risco", "35 años", ["Ver películas"])
+var paty = new MiembroSquad("Patricia Zabala", "28 años", ["Cantar"])
 
-function compas(){
-	var gaby = new MiembroSquad("Gaby Bravo", "28 años", ["Pintar", "Ver anime", "Jugar Aion"])
-	arr.push(gaby);
-	var karina = new MiembroSquad("Karina Laroze", "21 años", ["Bailar"])
-	arr.push(karina);
-	var mily = new MiembroSquad("Mily Rivas", "18 años", ["Leer", "Ver películas"])
-	arr.push(mily);
-	var marcela = new MiembroSquad("Marcela Orellana", "31 años", ["Andar en bicicleta", "Ver series coreanas"])
-	arr.push(marcela);
-	var angela = new MiembroSquad("Angela Aliaga", "32 años", ["Leer", "Escuchar música", "Jugar Donkey Kong Country"])
-	arr.push(angela);
-	var dani = new MiembroSquad("Daniela Risco", "35 años", ["Ver películas"])
-	arr.push(dani);
-	var paty = new MiembroSquad("Patricia Zabala", "28 años", ["Cantar"])
-	arr.push(paty);
+var squad = [gaby, karina, mily, marcela, angela, dani, paty];
 
-mostrar();
-}
+squad.forEach(function(element){element.mostrar();});
 
-function mostrar(){
-var imprimir = document.getElementById("compas");
-
-arr.forEach(function(el){
-var divVacio = document.createElement("div")
-divVacio.innerHTML += "<br>" + "<b>Nombre:</b> " + el.nombreApellido + "<br><b>Edad:</b> " + el.edad + "<br><b>Hobbies:</b><br>";
-imprimir.appendChild(divVacio) 
-var lista = document.createElement("li");
-lista.innerHTML = el.hobbies.forEach(function(e){
-divVacio.innerHTML += "<li>" + e + "</li>";
-});
-	
-var caja = document.createElement("textarea");
-caja.setAttribute("cols","50");
-caja.setAttribute("rows","4");
-divVacio.appendChild(caja);
-
-var botonComentar = document.createElement("button");
-botonComentar.setAttribute("onclick","crearComentario()");
-var nombreBoton = document.createTextNode("Dejar Comentario");
-botonComentar.appendChild(nombreBoton);
-divVacio.appendChild(botonComentar);
-});
-}
-
-compas();
